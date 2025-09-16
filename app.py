69dad6916e2c3e6fea0a174ba449c701e9a426e2
mike chess <mikecook450@gmail.com>
2025-09-16 11:08:24 -0400

    get_conversational_chain function

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PyPDF import PdfReader
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
@@ -35,6 +35,24 @@ def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
+
+
+def get_conversation_chain():
+    prompt_template = """
+        Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+        Context:\n {context}?\n
+        Question: \n{question}\n
+        Answer:
+    """
+
+    model=ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.3)
+
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+
+
 
 
 
