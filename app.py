44cb18b19815a14184d73e7880f433f5e45f8d3b
mike chess <mikecook450@gmail.com>
2025-09-16 11:15:00 -0400

    user_input function

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_vector_store(text_chunks):
     vector_store.save_local("faiss_index")
 
 
-def get_conversation_chain():
+def get_conversational_chain():
     prompt_template = """
         Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
         Context:\n {context}?\n
@@ -50,6 +50,25 @@ def get_conversation_chain():
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
 
 
 
