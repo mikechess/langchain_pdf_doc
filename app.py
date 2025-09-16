c267ef99938729669fe48d0591dcb3efd0515f27
mike chess <mikecook450@gmail.com>
2025-09-16 10:39:05 -0400

    get_pdf_text function

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+import streamlit as st
+from PyPDF import PdfReader
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
 
+load_dotenv()
 
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-
-
-
-
-
-
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 
 
